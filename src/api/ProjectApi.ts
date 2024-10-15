f74e70db2009074db7455a194020e0870284278b
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-10-15 16:44:08 -0300

    validate api response with zod

--- src/api/ProjectApi.ts
+++ src/api/ProjectApi.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "@/lib/axios";
-import { ProjectFormData } from "@/types/index";
+import { dashboardProjectSchema, ProjectFormData } from "@/types/index";
 
 export async function createProject(formData: ProjectFormData) {
   try {
@@ -16,7 +16,11 @@ export async function createProject(formData: ProjectFormData) {
 export async function getProjects() {
   try {
     const { data } = await api.get("/projects");
-    return data;
+    const response = dashboardProjectSchema.safeParse(data);
+
+    if (response.success) {
+      return response.data;
+    }
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -10,23 +10,33 @@ export default function DashboardView() {
 
   if (isLoading) return <p className="text-2xl text-center">Cargando...</p>;
 
-  console.log("data:", data);
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">Mis Proyectos</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          Maneja y administra tus proyectos
+        </p>
 
-  return (
-    <>
-      <h1 className="text-5xl font-black">Mis Proyectos</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">
-        Maneja y administra tus proyectos
-      </p>
+        <nav className="my-5">
+          <Link
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            to="/projects/create"
+          >
+            Nuevo Proyecto
+          </Link>
+        </nav>
 
-      <nav className="my-5">
-        <Link
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-          to="/projects/create"
-        >
-          Nuevo Proyecto
-        </Link>
-      </nav>
-    </>
-  );
+        {data.length ? (
+          <p>Sí hay proyectos</p>
+        ) : (
+          <p className="text-center py-20">
+            No hay proyectos aún {""}
+            <Link to="/projects/create" className="text-fuchsia-500 font-bold">
+              Crear Proyecto
+            </Link>
+          </p>
+        )}
+      </>
+    );
 }
