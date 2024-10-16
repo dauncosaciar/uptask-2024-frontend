82406a75352746d40075b14703911501a199f43f
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-10-16 09:52:34 -0300

    create edit project form

--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -1,18 +1,21 @@
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getProjectById } from "@/api/ProjectApi";
+import EditProjectForm from "@/components/projects/EditProjectForm";
 
 export default function EditProjectView() {
   const params = useParams();
   const projectId = params.projectId!;
 
-  const { data, isLoading, error, isError } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["editProject", projectId],
     queryFn: () => getProjectById(projectId),
     retry: false
   });
 
-  console.log("data:", data);
+  if (isLoading) return <p className="text-2xl text-center">Cargando...</p>;
 
-  return <div>EditProjectView</div>;
+  if (isError) return <Navigate to="/404" />;
+
+  if (data) return <EditProjectForm />;
 }
