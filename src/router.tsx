8558128fa0ccd60ef1b2c564291528a11771049b
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-10-17 16:53:17 -0300

    create route to project details view

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "@/layouts/AppLayout";
 import DashboardView from "@/views/DashboardView";
 import CreateProjectView from "@/views/projects/CreateProjectView";
-import EditProjectView from "./views/projects/EditProjectView";
+import EditProjectView from "@/views/projects/EditProjectView";
+import ProjectDetailsView from "@/views/projects/ProjectDetailsView";
 
 export default function Router() {
   return (
@@ -11,6 +12,7 @@ export default function Router() {
         <Route element={<AppLayout />}>
           <Route path="/" element={<DashboardView />} index />
           <Route path="/projects/create" element={<CreateProjectView />} />
+          <Route path="/projects/:projectId" element={<ProjectDetailsView />} />
           <Route
             path="/projects/:projectId/edit"
             element={<EditProjectView />}
