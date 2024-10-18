14fac334e39cf66822dcae4308f68f08f7533dbf
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-10-18 09:52:31 -0300

    add first details to project details view

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -1,3 +1,43 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectById } from "@/api/ProjectApi";
+import AddTaskModal from "@/components/tasks/AddTaskModal";
+
 export default function ProjectDetailsView() {
-  return <div>ProjectDetailsView</div>;
+  const navigate = useNavigate();
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["editProject", projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: false
+  });
+
+  if (isLoading) return <p className="text-2xl text-center">Cargando...</p>;
+
+  if (isError) return <Navigate to="/404" />;
+
+  if (data) {
+    return (
+      <>
+        <h1 className="text-5xl font-black">{data.projectName}</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          {data.description}
+        </p>
+
+        <nav className="my-5 flex gap-3">
+          <button
+            type="button"
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            onClick={() => navigate("?newTask=true")}
+          >
+            Agregar Tarea
+          </button>
+        </nav>
+
+        <AddTaskModal />
+      </>
+    );
+  }
 }
