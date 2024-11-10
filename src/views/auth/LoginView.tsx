bd6823dd0747c8916354d02f0da54f696b4b0670
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-11-10 12:34:11 -0300

    add api authenticate user method

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 import { UserLoginForm } from "@/types/index";
 import ErrorMessage from "@/components/ErrorMessage";
+import { authenticateUser } from "@/api/AuthApi";
 
 export default function LoginView() {
   const initialValues: UserLoginForm = {
@@ -15,7 +18,17 @@ export default function LoginView() {
     formState: { errors }
   } = useForm({ defaultValues: initialValues });
 
-  const handleLogin = (formData: UserLoginForm) => {};
+  const { mutate } = useMutation({
+    mutationFn: authenticateUser,
+    onError: error => {
+      toast.error(error.message);
+    },
+    onSuccess: data => {
+      toast.success(data);
+    }
+  });
+
+  const handleLogin = (formData: UserLoginForm) => mutate(formData);
 
   return (
     <>
