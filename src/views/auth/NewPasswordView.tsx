b62ee668fe762bfaef9c1d343de38ef94dbc0cf5
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-11-14 18:54:18 -0300

    update password with token

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -24,7 +24,7 @@ export default function NewPasswordView() {
           setIsValidToken={setIsValidToken}
         />
       ) : (
-        <NewPasswordForm />
+        <NewPasswordForm token={token} />
       )}
     </>
   );
