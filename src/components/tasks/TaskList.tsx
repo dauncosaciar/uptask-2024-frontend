5150b7d44729bab3a2ee425905ae6bfd6d36993a
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-12-19 16:15:34 -0300

    install dndkit; create DropTask component

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,6 +1,7 @@
 import { Task } from "@/types/index";
 import TaskCard from "./TaskCard";
 import { statusTranslations } from "@/locales/es";
+import DropTask from "./DropTask";
 
 type TaskListProps = {
   tasks: Task[];
@@ -46,6 +47,9 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
             >
               {statusTranslations[status]}
             </h3>
+
+            <DropTask />
+
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">
