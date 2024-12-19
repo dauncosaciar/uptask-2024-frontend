5a0c6ae1c2a9af573c3da2ad5e5e6a81e7d747e7
Daniel Alejandro Uncos Aciar <daniel.alejandro.uncos.aciar@gmail.com>
2024-12-19 16:42:21 -0300

    working with useDraggable hook

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useDraggable } from "@dnd-kit/core";
 import {
   Menu,
   MenuButton,
@@ -19,6 +20,10 @@ type TaskCardProps = {
 };
 
 export default function TaskCard({ task, canEdit }: TaskCardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: task._id
+  });
+
   const navigate = useNavigate();
   const params = useParams();
   const projectId = params.projectId!;
@@ -36,9 +41,17 @@ export default function TaskCard({ task, canEdit }: TaskCardProps) {
     }
   });
 
+  const style = transform ? {} : undefined;
+
   return (
     <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
-      <div className="min-w-0 flex flex-col gap-y-4">
+      <div
+        {...listeners}
+        {...attributes}
+        ref={setNodeRef}
+        style={style}
+        className="min-w-0 flex flex-col gap-y-4"
+      >
         <button
           type="button"
           className="text-xl font-bold text-slate-600 text-left"
